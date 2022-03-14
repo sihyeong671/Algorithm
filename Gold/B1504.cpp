f9b74fe6520f066f4e35d14c0c8728166568c6aa
sihyeong671 <bshlab671@naver.com>
2022-03-14 15:03:28 +0900

    PS

---
+++ Gold/B1504.cpp
@@ -0,0 +1,18 @@
+//특정한 최단 경로
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, E, v1, v2;
+
+int main(){
+  cin >> N >> E;
+  int a, b ,c;
+  for(int i = 0; i < N; ++i){
+    cin >> a >> b >> c;
+  }
+  cin >> v1 >> v2;
+  return 0;
+}
--- Gold/B1707.cpp
+++ Gold/B1707.cpp
@@ -2,20 +2,54 @@
 
 #include<iostream>
 #include<vector>
-#include<vector>
 
 using namespace std;
 
 int K, V, E;
+bool state;
+void dfs(int past, int current, vector<bool> &list, vector<vector<int>> &graph);
 
 int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
   cin >> K;
 
   for(int i = 0; i < K; ++i){
     cin >> V >> E;
-    for(int j = 0; j < E; ++j){
 
+    int u, v;
+    vector<vector<int>> graph(V+1);
+    vector<bool> check(V+1, false);
+    state = false;
+    for(int j = 1; j <= E; ++j){
+      cin >> u >> v;
+      graph[u].push_back(v);
+      graph[v].push_back(u);
     }
+    
+    for(int j = 1; j <= V; ++j){
+      if(!check[j]) dfs(0, j, check, graph);
+    }
+
+    if(state) cout << "NO\n";
+    else cout << "YES\n";
   }
-  
+}
+
+void dfs(int past, int current, vector<bool> &list, vector<vector<int>> &graph){
+  if(state) return;
+
+  if(list[current]){
+    state = true;
+    return;
+  }
+
+  list[current] = true;
+  cout << "graph : " << graph[current].size() << endl;
+  for(auto n : graph[current]){
+    if(n == past) continue;
+    dfs(current, n, list, graph);
+  }
 }
