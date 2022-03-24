04bcc991173d26f03b0013bafc6af025ed8d1f40
sihyeong671 <bshlab671@naver.com>
2022-03-24 14:44:41 +0900

    PS

--- Gold/B1504.cpp
+++ Gold/B1504.cpp
@@ -2,17 +2,41 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+typedef struct{
+  int v;
+  int cost;
+  bool v1_find;
+  bool v2_find;
+}info;
+
+struct compare{
+  bool operator()(info &a, info &b){
+    return a.cost > b.cost;
+  }
+};
 
 int N, E, v1, v2;
 
 int main(){
+
   cin >> N >> E;
+
+  vector<vector<int>> graph(N+1, vector<int>(N+1, -1));
+  priority_queue<info, vector<info>, compare> q;
   int a, b ,c;
+
   for(int i = 0; i < N; ++i){
     cin >> a >> b >> c;
+    graph[a][b] = c;
+    graph[b][a] = c;
   }
+  
   cin >> v1 >> v2;
+
+
   return 0;
 }
